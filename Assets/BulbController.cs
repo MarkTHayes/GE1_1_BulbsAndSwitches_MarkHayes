74566365fdedb486413f0315c4fa6d33fe71598c
MarkTHayes <markh4387@gmail.com>
2019-01-17 12:42:11 +0000

    Initial commit

---
+++ Assets/BulbController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbController : MonoBehaviour {
+
+    public Animator bulbAnimator;
+
+    public SwitchController manualSwitch;
+    public SwitchController autoSwitch;
+
+    //gitHub change test
+
+	// Use this for initialization
+	void Start () {
+
+        bulbAnimator = gameObject.GetComponent<Animator>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (autoSwitch.switchOff == false || manualSwitch.switchOff == false)
+        {
+
+            bulbAnimator.SetBool("BulbOn", true);
+            
+        }
+        else if (autoSwitch.switchOff && manualSwitch.switchOff)
+        {
+
+            bulbAnimator.SetBool("BulbOn", false);
+
+        }
+
+
+
+    }
+}
